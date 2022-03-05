61f53b4a3782f3724aeb97bb048df31d59307c6f
IamMrandrew <andrewliyanlap000824@gmail.com>
2022-03-05 19:58:22 +0800

    Initial Commit

---
+++ ARDemo/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ARDemo
+//
+//  Created by Andrew Li on 5/3/2022.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
